097733113769f95cf94dc15bebb198d54f05ead6
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2021-08-07 23:20:37 -0300

    create colors

---
+++ src/Global/Styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+        primary: '#5636D3',
+        secondary: '#FF872C',
+        secondary__light: 'rgba(255, 135, 44, .3)',
+        success: '#12A454',
+        success__light: 'rgba(18, 164, 84, 0.5)',
+        attention: '#E83F5B',
+        attention__light: 'rgba(232, 63, 91, 0.5)',
+        shape: '#FFFFFF',
+        title: '#363F5F',
+        text: '#9696B2',
+        background: '#F0F2F5'
+    }
+}
