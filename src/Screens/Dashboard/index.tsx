713679fcedfb6472169177deecd8f120ff3707b8
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2021-08-08 00:41:53 -0300

    working on application Header

--- src/Screens/Dashboard/index.tsx
+++ src/Screens/Dashboard/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+} from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/36776702?v=4' }}
+                        />
+
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Guilherme</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
